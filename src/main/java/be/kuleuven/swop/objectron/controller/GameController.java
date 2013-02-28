fec771218344b851df7380167de1dad7ba083893
Kasper Vervaecke <kasper.vervaecke@student.kuleuven.be>
2013-02-28 15:33:26 +0100

    Added some comments

--- src/main/java/be/kuleuven/swop/objectron/controller/GameController.java
+++ src/main/java/be/kuleuven/swop/objectron/controller/GameController.java
@@ -1,10 +1,12 @@
 package be.kuleuven.swop.objectron.controller;
 
+import be.kuleuven.cs.som.annotate.Basic;
 import be.kuleuven.swop.objectron.model.*;
 
 import java.util.List;
 
 /**
+ * A class of game controller implementations.
  * @author : Nik Torfs
  *         Date: 22/02/13
  *         Time: 00:11
@@ -13,10 +15,18 @@ public class GameController {
 
     private HumanPlayer currentPlayer;
 
+    /**
+     * Return the current player.
+     */
+    @Basic
     public HumanPlayer getCurrentPlayer() {
         return currentPlayer;
     }
 
+    /**
+     * Set the current player.
+     */
+    @Basic
     public void setCurrentPlayer(HumanPlayer currentPlayer) {
         this.currentPlayer = currentPlayer;
     }
@@ -26,7 +36,13 @@ public class GameController {
         throw new RuntimeException("Unimplemented");
     }
 
-    //TODO showInventory
+    /**
+     * Retrieve the inventory of the current player.
+     * @throws InventoryEmptyException
+     *         The current player's inventory is empty
+     *         | getCurrentPlayer().getInventory().isEmpty()
+     * @return A list of the items in the inventory of the current player.
+     */
     public List<Item> showInventory(){
         List<Item> inventory = getCurrentPlayer().getInventory().getItems();
         if(!inventory.isEmpty()) {
@@ -36,13 +52,27 @@ public class GameController {
         }
     }
 
-    //TODO selectItem
+    /**
+     * Retrieve an item from the player's inventory and set it as the currently selected item
+     * @param identifier
+     *        The identifier of the item.
+     * @post  The item is selected
+     *        | currentPlayer.getCurrentlySelectedItem()
+     *        |  == currentPlayer.getInventory().retrieveItem(identifier)
+     */
     public void selectItem(int identifier){
         Item selectedItem = currentPlayer.getInventory().retrieveItem(identifier);
         currentPlayer.setCurrentlySelectedItem(selectedItem);
     }
 
-    //TODO useCurrentItem
+    /**
+     * Use the currently selected item
+     * @post   The item is removed from the player's inventory
+     *         | !currentPlayer.getInventory().contains(currentPlayer.getCurrentlySelectedItem())
+     * @post   The player's available actions is reduced by 1
+     *         | new.currentPlayer.getAvailableActions() = currentPlayer.getAvailableActions()-1
+     * @return A boolean to indicate whether the item was successfully used
+     */
     public boolean useCurrentItem(){
         Item currentItem = currentPlayer.getCurrentlySelectedItem();
         currentPlayer.getInventory().removeItem(currentItem.getIdentifier());
