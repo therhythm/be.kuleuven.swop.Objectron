4b2cb2c6734db927a7dda1fc8017bdf74a7ad497
Thomas Vandebuerie <thomas.vandebuerie@student.kuleuven.be>
2013-02-28 15:19:52 +0100

    added scenario test for end turn, added gameoverexception

---
+++ src/main/java/be/kuleuven/swop/objectron/model/GameOverException.java
@@ -0,0 +1,21 @@
+package be.kuleuven.swop.objectron.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomas
+ * Date: 28/02/13
+ * Time: 15:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameOverException extends Exception {
+    /**
+    * Initialize this exception with a given message.
+    * @param message
+    *        The message for this exception.
+    * @post  The new error message for this exception equals the given message.
+    *        | new.getMessage == message
+    */
+    public GameOverException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/scenario/TestUC_End_Turn.java
@@ -0,0 +1,44 @@
+package scenario;
+
+
+import be.kuleuven.swop.objectron.GameState;
+import be.kuleuven.swop.objectron.controller.GameController;
+import be.kuleuven.swop.objectron.gui.GameView;
+import be.kuleuven.swop.objectron.model.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class TestUC_End_Turn {
+
+    private GameController controller;
+    private Player player;
+    private GameState state;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        GameState state = new GameState("jos", "piet", 10, 10);
+        GameController controller = new GameController(state);
+        GameView view = new GameView(controller, 10, 10);
+
+    }
+
+    @Test(expected = GameOverException.class)
+    private void test_end_turn_no_moves(){
+        controller.endTurn();
+    }
+
+    @Test
+    private void test_end_turn_with_move(){
+        Player oldPlayer = state.getCurrentPlayer();
+        controller.move(Direction.UP);
+        controller.endTurn();
+        assertFalse(state.getCurrentPlayer().equals(oldPlayer));
+
+    }
+}
