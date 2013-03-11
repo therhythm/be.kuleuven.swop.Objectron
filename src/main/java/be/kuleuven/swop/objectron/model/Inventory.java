f8136b0bee9b85346725cbd45e0d9da0cf1cae42
Nik Torfs <niktorfs@gmail.com>
2013-03-11 23:08:07 +0100

    Removed inventory interface

--- src/main/java/be/kuleuven/swop/objectron/model/Inventory.java
+++ src/main/java/be/kuleuven/swop/objectron/model/Inventory.java
@@ -3,38 +3,57 @@ package be.kuleuven.swop.objectron.model;
 import be.kuleuven.swop.objectron.model.exception.InventoryFullException;
 import be.kuleuven.swop.objectron.model.item.Item;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @author : Nik Torfs
  *         Date: 21/02/13
- *         Time: 23:54
+ *         Time: 23:51
  */
-public interface Inventory {
-    /**
-     * @return
-     */
-    List<Item> getItems();
+public class Inventory {
+    private static final int LIMIT = 6;
+
+    private List<Item> items = new ArrayList<Item>();
 
     /**
-     * Takes (removes) an item from the inventory and returns it.
-     *
-     * @param identifier: the item identifier
-     * @return the item to retrieve
-     */
-    Item retrieveItem(int identifier);
+    * @return
+    */
+    public List<Item> getAllItems() {
+        return Collections.unmodifiableList(items);
+    }
 
     /**
-     * Add an item to the inventory
-     *
-     * @param itemToAdd
-     */
-    void addItem(Item itemToAdd) throws InventoryFullException;
+    * Takes (removes) an item from the inventory and returns it.
+    *
+    * @param identifier: the item identifier
+    * @return the item to retrieve
+    */
+    public Item retrieveItem(int identifier) {
+        return items.get(identifier);
+    }
+
+    private boolean isLimitReached() {
+        return this.items.size() >= LIMIT;
+    }
 
     /**
-     * Remove an item from the inventory
-     *
-     * @param item
-     */
-    void removeItem(Item item);
+    * Add an item to the inventory
+    *
+    * @param itemToAdd
+    */
+    public void addItem(Item itemToAdd) throws InventoryFullException {
+        if(isLimitReached()) {
+            throw new InventoryFullException("Inventory full");
+        }
+        items.add(itemToAdd);
+    }
+
+    /**
+    * Remove an item from the inventory
+    *
+    * @param item
+    */
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
 }
