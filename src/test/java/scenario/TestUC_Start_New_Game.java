2f2c2ff0e28344789662e9e0a93bc300aeb4ddfe
Peter <peter.bosmans.pb@gmail.com>
2013-03-13 14:30:31 +0100

    TestUC_Start_New_Game overbodige imports verwijderd

--- src/test/java/scenario/TestUC_Start_New_Game.java
+++ src/test/java/scenario/TestUC_Start_New_Game.java
@@ -1,19 +1,12 @@
 package scenario;
 
-import be.kuleuven.swop.objectron.GameStateImpl;
-import be.kuleuven.swop.objectron.domain.Player;
-import be.kuleuven.swop.objectron.domain.Square;
+
 import be.kuleuven.swop.objectron.domain.exception.GridTooSmallException;
 import be.kuleuven.swop.objectron.domain.exception.InventoryEmptyException;
-import be.kuleuven.swop.objectron.domain.exception.NotEnoughActionsException;
-import be.kuleuven.swop.objectron.domain.item.LightMine;
 import be.kuleuven.swop.objectron.handler.StartGameHandler;
-import be.kuleuven.swop.objectron.handler.UseItemHandler;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Created with IntelliJ IDEA.
