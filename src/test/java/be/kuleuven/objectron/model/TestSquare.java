1adf3cd14f5f7d384c20cc7b0e0198a216ad5ee0
Thomas Vandebuerie <thomas.vandebuerie@student.kuleuven.be>
2013-04-11 14:28:01 +0200

    Power failure test, fixed remaining actions bug, huzzah!

--- src/test/java/be/kuleuven/objectron/model/TestSquare.java
+++ src/test/java/be/kuleuven/objectron/model/TestSquare.java
@@ -1,12 +1,18 @@
 package be.kuleuven.objectron.model;
 
+import be.kuleuven.swop.objectron.domain.Direction;
 import be.kuleuven.swop.objectron.domain.Player;
 
-import be.kuleuven.swop.objectron.domain.exception.GridTooSmallException;
-import be.kuleuven.swop.objectron.domain.exception.SquareOccupiedException;
+import be.kuleuven.swop.objectron.domain.Settings;
+import be.kuleuven.swop.objectron.domain.exception.*;
+import be.kuleuven.swop.objectron.domain.gamestate.GameState;
+import be.kuleuven.swop.objectron.domain.grid.Grid;
+import be.kuleuven.swop.objectron.domain.grid.GridFactory;
 import be.kuleuven.swop.objectron.domain.item.LightMine;
 import be.kuleuven.swop.objectron.domain.square.Square;
+import be.kuleuven.swop.objectron.domain.util.Dimension;
 import be.kuleuven.swop.objectron.domain.util.Position;
+import be.kuleuven.swop.objectron.handler.MovePlayerHandler;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -19,31 +25,46 @@ import static org.junit.Assert.*;
  * To change this template use File | Settings | File Templates.
  */
 public class TestSquare {
-    private Square square;
+    private Square currentSquare;
     private Player player;
+    private GameState state;
+    private MovePlayerHandler movePlayerHandler;
 
     @Before
     public void setUp()throws GridTooSmallException, SquareOccupiedException {
-        square = new Square(new Position(5, 5));
-        square.setActiveItem(new LightMine());
-        player = new Player("test",new Square(new Position(5, 4)));
-    }
-   /* TODO
-    @Test
-    public void test_step_on_active_item(){
-        assertTrue(square.hasActiveItem());
-        square.stepOn(player);
-        assertFalse(square.hasActiveItem());
+        Dimension dimension = new Dimension(10, 10);
+        Grid grid = GridFactory.gridWithoutWalls(dimension, new Position(0, 9), new Position(9, 0));
+        state = new GameState("p1", "p2", dimension, grid);
+        player = state.getCurrentPlayer();
+        currentSquare = player.getCurrentSquare();
+        movePlayerHandler = new MovePlayerHandler(state);
     }
 
     @Test
-    public void test_square_lose_power(){
-        Square otherSquare = new Square(new Position(5, 3));
-        Player otherPlayer = new Player("tester", otherSquare);
-        otherSquare.receivePowerFailure();
-        otherSquare.newTurn(otherPlayer);
-        assertEquals(otherPlayer.getAvailableActions(),2);
-    } */
+    public void testStartUnpowered(){
+        state.endTurn();
+        currentSquare.receivePowerFailure();
+        state.endTurn();
+        assertEquals(Settings.PLAYER_ACTIONS_EACH_TURN - 1, state.getCurrentTurn().getActionsRemaining());
+    }
+
+    @Test
+    public void testStepOnUnpoweredSquare() throws GameOverException, InvalidMoveException, NotEnoughActionsException{
+        currentSquare.getNeighbour(Direction.UP).receivePowerFailure();
+        assertEquals(player, state.getCurrentPlayer());
+        movePlayerHandler.move(Direction.UP);
+        assertNotEquals(player, state.getCurrentPlayer());
+    }
+
+    @Test
+    public void testStepOnActiveUnpowered() throws SquareOccupiedException, NotEnoughActionsException, SquareOccupiedException, InvalidMoveException, GameOverException{
+        currentSquare.getNeighbour(Direction.UP).setActiveItem(new LightMine());
+        currentSquare.getNeighbour(Direction.UP).receivePowerFailure();
+        int remainingActionsAfterMove = state.getCurrentTurn().getActionsRemaining() - 1;
+        movePlayerHandler.move(Direction.UP);
+        state.endTurn();
+        assertEquals(4 - remainingActionsAfterMove, player.getRemainingPenalties());
+    }
 
 
 }
