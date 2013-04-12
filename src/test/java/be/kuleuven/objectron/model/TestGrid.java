2aac990ba030741b838cdc425ed49921d5f57a8a
Peter <peter.bosmans.pb@gmail.com>
2013-04-12 17:04:17 +0200

    iets in testGrid aangepast

--- src/test/java/be/kuleuven/objectron/model/TestGrid.java
+++ src/test/java/be/kuleuven/objectron/model/TestGrid.java
@@ -116,6 +116,7 @@ public class TestGrid {
     @Test
     public void test_charged_identity_disc() throws GridTooSmallException {
         grid = GridFactory.gridWithoutWallsPowerFailures(dimension, new Position(0,9), new Position(9,0));
+      int aantal = 0;
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 for (Item item : grid.getSquareAtPosition(new Position(i, j)).getAvailableItems()) {
@@ -125,12 +126,14 @@ public class TestGrid {
                         System.out.println("distance from player1: " + distanceFromPlayer1);
                         System.out.println("distance from player2: " + distanceFromPlayer2);
                         assertTrue(Math.abs(distanceFromPlayer1 - distanceFromPlayer2) < 2);
+                        aantal++;
 
                     }
 
                 }
             }
         }
+        assertTrue(aantal==1);
     }
 
     @Test
