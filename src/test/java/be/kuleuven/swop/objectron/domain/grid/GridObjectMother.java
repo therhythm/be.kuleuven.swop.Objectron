a0fe75439ff72e10161f0ac8794e0eb366236379
Nik Torfs <niktorfs@gmail.com>
2013-05-23 01:49:36 +0200

    removing unused methods

--- src/test/java/be/kuleuven/swop/objectron/domain/grid/GridObjectMother.java
+++ src/test/java/be/kuleuven/swop/objectron/domain/grid/GridObjectMother.java
@@ -35,34 +35,4 @@ public class GridObjectMother {
         builder.initGrid(0);
         return builder.buildGrid();
     }
-
-    // construct a grid without power , walls or items
-    public static Grid unpoweredGridWithoutWallsWithoutItems(GridBuilder builder) throws GridTooSmallException {
-        builder.initGrid(100);
-        Grid g = builder.buildGrid();
-
-        // powerfailure on every square
-        for (int x = 0; x < g.getDimension().getWidth(); x++) {
-            for (int y = 0; y < g.getDimension().getHeight(); y++) {
-              //  new PrimaryPowerFailure(g.getSquareAtPosition(new Position(x, y)));
-            }
-        }
-        return g;
-    }
-
-    // construct a grid without power
-    public static Grid unpoweredGrid(GridBuilder builder) throws GridTooSmallException {
-        builder.initGrid(100);
-        builder.buildWalls();
-        builder.buildItems();
-        Grid g = builder.buildGrid();
-
-        // powerfailure on every square
-        for (int x = 0; x < g.getDimension().getWidth(); x++) {
-            for (int y = 0; y < g.getDimension().getHeight(); y++) {
-               // new PrimaryPowerFailure(g.getSquareAtPosition(new Position(x, y)));
-            }
-        }
-        return g;
-    }
 }
