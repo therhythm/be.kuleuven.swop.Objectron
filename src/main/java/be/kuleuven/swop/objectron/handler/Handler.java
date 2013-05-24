ed4030c74b955eea5dcf180e64ac5b3017a47b7f
Kasper Vervaecke <kasper.vervaecke@student.kuleuven.be>
2013-05-24 14:07:55 +0200

    Commenteroni

--- src/main/java/be/kuleuven/swop/objectron/handler/Handler.java
+++ src/main/java/be/kuleuven/swop/objectron/handler/Handler.java
@@ -3,6 +3,7 @@ package be.kuleuven.swop.objectron.handler;
 import be.kuleuven.swop.objectron.domain.gamestate.Game;
 
 /**
+ * An abstract class of Handlers involving a Game.
  * @author : Nik Torfs
  *         Date: 12/03/13
  *         Time: 01:14
@@ -10,6 +11,13 @@ import be.kuleuven.swop.objectron.domain.gamestate.Game;
 public abstract class Handler {
     protected Game game;
 
+    /**
+     * Initialize a new Handler with the given Game.
+     * @param game
+     *        The game for this new Handler.
+     * @post  This Handler is initialized with the given Game.
+     *        | new.this.game == game
+     */
     protected Handler(Game game) {
         this.game = game;
     }
--- src/main/java/be/kuleuven/swop/objectron/handler/StartGameHandler.java
+++ src/main/java/be/kuleuven/swop/objectron/handler/StartGameHandler.java
@@ -13,12 +13,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * A class of StartGameHandlers involving GameStartViewModels.
  * @author : Nik Torfs
  *         Date: 13/03/13
  *         Time: 01:14
  */
 public class StartGameHandler {
 
+    /**
+     * Start a new race game.
+     * @param playerNames
+     *        A list of player names.
+     * @param dimension
+     *        The dimensions of the grid.
+     * @param file
+     *        An optional file containing the grid.
+     * @return A GameStartViewModel.
+     * @throws GridTooSmallException
+     *         The specified dimensions result in a grid that is too small.
+     *         | dimension.getWidth < 10 || dimension.getHeight < 10
+     * @throws InvalidFileException
+     *         Generating a grid from the file would result in an invalid grid.
+     *         | file.hasUnreachableSquares() || file.StartingPositions != playerNames.size()
+     * @throws NumberOfPlayersException
+     *         The number of players is invalid for this grid.
+     *         | playerNames.size() < 2 || playerNames.size() > 9
+     */
     public GameStartViewModel startNewRaceGame(List<String> playerNames,
                                                Dimension dimension, String file) throws GridTooSmallException, InvalidFileException, NumberOfPlayersException {
         GameBuilder gameBuilder = new RaceGameBuilder(playerNames, dimension);
@@ -46,6 +66,25 @@ public class StartGameHandler {
                 (), game.getGrid().getWalls(), game.getGrid().getItems(), game.getGrid().getEffects(), game);
     }
 
+    /**
+     * Start a new Capture The Flag game
+     * @param playerNames
+     *        A list of player names.
+     * @param dimension
+     *        The dimensions of the grid.
+     * @param file
+     *        An optional file containing the grid.
+     * @return A GameStartViewModel.
+     * @throws GridTooSmallException
+     *         The specified dimensions result in a grid that is too small.
+     *         | dimension.getWidth < 10 || dimension.getHeight < 10
+     * @throws InvalidFileException
+     *         Generating a grid from the file would result in an invalid grid.
+     *         | file.hasUnreachableSquares() || file.StartingPositions != playerNames.size()
+     * @throws NumberOfPlayersException
+     *         The number of players is invalid for this grid.
+     *         | playerNames.size() < 2 || playerNames.size() > 9
+     */
     public GameStartViewModel startNewCTFGame(List<String> playerNames,
                                               Dimension dimension, String file) throws GridTooSmallException, InvalidFileException, NumberOfPlayersException {
         GameBuilder gameBuilder = new CTFGameBuilder(playerNames, dimension);
